9e4fbaa92c1fe50b68ab115dbddbdc3d8b7a8571
Wessel Konstantinov <wes_kon1@hotmail.com>
2023-06-20 19:11:00 +0200

    Refactor script

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const shareButton = document.querySelector('[data-js="share-button"]');
-const shareSection = document.querySelector('[data-js="share-section"]');
 
-shareButton.addEventListener("click", (e) => {
+shareButton.addEventListener("click", () => {
   const isOpen = shareButton.getAttribute("aria-expanded") === "true";
   shareButton.setAttribute("aria-expanded", !isOpen);
-  isOpen
-    ? shareButton.setAttribute("aria-label", "Open share options")
-    : shareButton.setAttribute("aria-label", "Close share options");
+
+  if (isOpen) {
+    shareButton.setAttribute("aria-label", "Open share options");
+  } else {
+    shareButton.setAttribute("aria-label", "Close share options");
+  }
 });
