df98f65b8a3a5c6881f78dd9b5b95fa2fa61bd9b
Wessel Konstantinov <wes_kon1@hotmail.com>
2023-06-21 20:15:18 +0200

    Add escape functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const shareButton = document.querySelector('[data-js="share-button"]');
+const shareSection = document.querySelector('[data-js="share-section"]');
 
 shareButton.addEventListener("click", () => {
   const isOpen = shareButton.getAttribute("aria-expanded") === "true";
@@ -9,4 +10,13 @@ shareButton.addEventListener("click", () => {
   } else {
     shareButton.setAttribute("aria-label", "Close share options");
   }
+});
+
+[shareButton, shareSection].forEach((element) => {
+  element.addEventListener("keyup", (e) => {
+    if (e.code === "Escape") {
+      shareButton.setAttribute("aria-expanded", false);
+      shareButton.setAttribute("aria-label", "Open share options");
+    }
+  });
 });
