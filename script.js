f4177cde3c4d01cf40b643392d3acf6ca4ce4ab2
Wessel Konstantinov <wes_kon1@hotmail.com>
2023-06-28 19:11:43 +0200

    Make aria-label persist in all states

--- script.js
+++ script.js
@@ -4,19 +4,12 @@ const shareSection = document.querySelector('[data-js="share-section"]');
 shareButton.addEventListener("click", () => {
   const isOpen = shareButton.getAttribute("aria-expanded") === "true";
   shareButton.setAttribute("aria-expanded", !isOpen);
-
-  if (isOpen) {
-    shareButton.setAttribute("aria-label", "Open share options");
-  } else {
-    shareButton.setAttribute("aria-label", "Close share options");
-  }
 });
 
 [shareButton, shareSection].forEach((element) => {
   element.addEventListener("keyup", (e) => {
     if (e.code === "Escape") {
       shareButton.setAttribute("aria-expanded", false);
-      shareButton.setAttribute("aria-label", "Open share options");
     }
   });
 });
